6fe59d0794a629fedbf2c152dbc53fda971be449
djye <djye2010@gmail.com>
2024-06-26 17:33:57 +0800

    the bundle gas fee needs to include the blob fee

--- miner/worker_builder.go
+++ miner/worker_builder.go
@@ -447,6 +447,12 @@ func (w *worker) simulateBundle(
 		}
 		txGasFees := new(big.Int).Mul(txGasUsed, effectiveTip)
 		bundleGasFees.Add(bundleGasFees, txGasFees)
+
+		if tx.Type() == types.BlobTxType {
+			blobFee := new(big.Int).SetUint64(receipt.BlobGasUsed)
+			blobFee.Mul(blobFee, receipt.BlobGasPrice)
+			bundleGasFees.Add(bundleGasFees, blobFee)
+		}
 		sysBalanceAfter := state.GetBalance(consensus.SystemAddress)
 		sysDelta := new(uint256.Int).Sub(sysBalanceAfter, sysBalanceBefore)
 		sysDelta.Sub(sysDelta, uint256.MustFromBig(txGasFees))
